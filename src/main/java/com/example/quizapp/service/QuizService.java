8bd8eb1368f87bc02504f350d40648f3f7aea9fd
gargpriyanka512 <gargpriyanka512@gmail.com>
2025-05-25 23:50:35 -0400

    Quiz are being created based on topics

--- src/main/java/com/example/quizapp/service/QuizService.java
+++ src/main/java/com/example/quizapp/service/QuizService.java
@@ -1,5 +1,9 @@
 package com.example.quizapp.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +11,8 @@ import org.springframework.stereotype.Service;
 
 import com.example.quizapp.dao.QuestionDao;
 import com.example.quizapp.dao.QuizDao;
+import com.example.quizapp.model.Question;
+import com.example.quizapp.model.QuestionWrapper;
 import com.example.quizapp.model.Quiz;
 
 @Service
@@ -24,5 +30,16 @@ public class QuizService {
 		quiz.setQuestions(questionDao.findRandomQuestionsByCategory(category, numQ));
 		quizDao.save(quiz);
 		return new ResponseEntity("Success", HttpStatus.CREATED);
+	}
+
+	public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(int id) {
+		Optional<Quiz> quiz = quizDao.findById(id);
+		List<Question> questionsFromDB = quiz.get().getQuestions();
+		List<QuestionWrapper> questionsForUser = new ArrayList<>();
+		for(Question q : questionsFromDB) {
+			QuestionWrapper qw = new QuestionWrapper(q.getId(),q.getQuestionTitle(),q.getOption1(),q.getOption2(),q.getOption3(),q.getOption4());
+			questionsForUser.add(qw);
+		}
+		return new ResponseEntity<>(questionsForUser, HttpStatus.OK);
 	}
 }
